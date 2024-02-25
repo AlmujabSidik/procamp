7e106977baf9a245afab3ec06ac57b2f7bcfb4bc
Almujab Sidik <almujabsidik@gmail.com>
2024-02-25 14:35:46 +0900

    Create Model, Migration , Seeder

---
+++ database/seeders/CampSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Camp;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CampSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $camps = [
+            [
+                'title' => 'GILA BELAJAR',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'BARU MULAI',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ]
+
+        ];
+
+        Camp::insert($camps);
+    }
+}
